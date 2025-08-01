98966000231a047be607b6b5306fe3be4f7f2be5
Vitor Jesus <Hakitsyu>
2025-08-01 00:22:37 -0300

    first method without predicate

--- internal/first.go
+++ internal/first.go
@@ -1,47 +1,20 @@
 package linq
 
+import "errors"
+
 type FirstFn[K interface{}] func(item K) bool
 
-type FirstEnumerator[K interface{}] struct {
-	Enumerator[K]
-	source Enumerator[K]
-	fn     FirstFn[K]
-}
+var (
+	ErrNoMatchFound = errors.New("no match found")
+)
 
-func (enumerator *FirstEnumerator[K]) Reset() {
-	enumerator.source.Reset()
-}
+func First[K interface{}](source Enumerable[K]) (K, error) {
+	enumerator := source.GetEnumerator()
 
-func (enumerator *FirstEnumerator[K]) MoveNext() bool {
-	for enumerator.source.MoveNext() {
-		if enumerator.fn(enumerator.Current()) {
-			return true
-		}
+	if enumerator.MoveNext() {
+		return enumerator.Current(), nil
 	}
 
-	return false
-}
-
-func (enumerator *FirstEnumerator[K]) Current() K {
-	return enumerator.source.Current()
-}
-
-type FirstEnumerable[K interface{}] struct {
-	Enumerable[K]
-	source Enumerable[K]
-	fn     FirstFn[K]
-}
-
-func (enumerable *FirstEnumerable[K]) GetEnumerator() Enumerator[K] {
-	return &FirstEnumerator[K]{
-		source: enumerable.source.GetEnumerator(),
-		fn:     enumerable.fn,
-	}
-}
-
-func First[K interface{}](source Enumerable[K], fn FirstFn[K]) Enumerable[K] {
-	return &FirstEnumerable[K]{
-		source: source,
-		fn:     fn,
-	}
+	var zero K
+	return zero, ErrNoMatchFound
 }
